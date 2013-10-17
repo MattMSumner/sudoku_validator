2774f88aacebf483df7fd0b9728184c4f9c76224
Matthew Sumner <matt.m.sumner@gmail.com>
2013-10-17 10:10:25 -0700

    adds gemfile, rspec and first pending spec.

---
+++ spec/sudoku_validator_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require_relative '../sudoku_validator.rb'
+
+describe SudokuValidator do
+  context '#validate' do
+    it 'validates sample files'
+  end
+end
---
+++ sudoku_validator.rb
@@ -0,0 +1,3 @@
+class SudokuValidator
+
+end
